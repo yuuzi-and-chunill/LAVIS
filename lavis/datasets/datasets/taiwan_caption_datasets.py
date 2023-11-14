7cd96bb1b0190535cb9b1b4124df7902b260376a
chunill <yces980096@gmail.com>
2023-11-15 01:22:23 +0800

    feat: add taiwan_caption dataset class file

---
+++ lavis/datasets/datasets/taiwan_caption_datasets.py
@@ -0,0 +1,41 @@
+import os
+from collections import OrderedDict
+
+from PIL import Image
+
+from lavis.datasets.datasets.base_dataset import BaseDataset
+
+import json
+import copy
+
+class TaiwanCaptionDatasets(BaseDataset):
+    def __init__(self, vis_processor, text_processor, vis_root, ann_paths):
+        """
+        vis_root (string): Root directory of images (e.g. coco/images/)
+        ann_root (string): directory to store the annotation file
+        """
+        
+        self.vis_root = vis_root
+        self.images_info = {}
+        
+        self.annotation = []
+        for ann_path in ann_paths:
+            images = json.load(open(ann_path, "r", encoding="utf-8"))
+            for image in images:
+                self.annotation.append(image)
+                
+        self.vis_processor = vis_processor
+        self.text_processor = text_processor
+
+    def __getitem__(self, index):
+        ann = self.annotation[index]
+
+        image_path = os.path.join(self.vis_root, ann+".jpg")
+        image = Image.open(image_path).convert("RGB")
+
+        image = self.vis_processor(image)
+
+        return {
+            "image" : image, 
+            "image_id" : ann
+        }
