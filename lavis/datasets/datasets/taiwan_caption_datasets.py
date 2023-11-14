8796c7e9e40e8b30f268a9f08034044bd7f9ca66
chunill <yces980096@gmail.com>
2023-11-15 04:01:32 +0800

    fix: KeyError

--- lavis/datasets/datasets/taiwan_caption_datasets.py
+++ lavis/datasets/datasets/taiwan_caption_datasets.py
@@ -22,20 +22,22 @@ class TaiwanCaptionDatasets(BaseDataset):
         for ann_path in ann_paths:
             images = json.load(open(ann_path, "r", encoding="utf-8"))
             for image in images:
-                self.annotation.append(image)
+                self.annotation.append(images[image])
                 
         self.vis_processor = vis_processor
         self.text_processor = text_processor
+        print("annotation:", self.annotation)
 
     def __getitem__(self, index):
         ann = self.annotation[index]
 
-        image_path = os.path.join(self.vis_root, ann+".jpg")
+        image_path = os.path.join(self.vis_root, "ID"+ann["id"]+".jpg")
         image = Image.open(image_path).convert("RGB")
 
         image = self.vis_processor(image)
 
         return {
             "image" : image, 
-            "image_id" : ann
+            "image_id" : ann["id"],
+            "text_input": ann["introduction"]
         }
